e3dd99da153034c1bb118472faffc7804cd43c33
Hudson Drozdowski <hudsondrozdowski@gmail.com>
2026-01-16 02:57:48 -0600

    Part 2 Step 4: Added the tests for the new attributes added in part 2.

--- tests/test_library_item.py
+++ tests/test_library_item.py
@@ -23,9 +23,10 @@ class TestLibraryItem(unittest.TestCase):
         title = "Book_Name"
         author = "Author_Name"
         genre = Genre.FICTION
+        item_id = 1
+        is_borrowed = True
 
-        #Act
-        target = LibraryItem(title, author, genre)
+        target = LibraryItem(item_id, title, author, genre, is_borrowed)
 
         #Assert
         #Checks that all the instance attributes match the inputs.
@@ -33,7 +34,56 @@ class TestLibraryItem(unittest.TestCase):
         self.assertEqual(author, target.author)
         self.assertEqual(genre, target.genre)
 
-        
+
+    def test_init_exception_when_is_borrowed_is_not_bool(self):
+        """Tests if the propper exception is raised
+        when is_borrowed is not a bool""" 
+        #Arange
+        #These variables will be input into an instance of LibraryItem
+        #to test for a ValueError when is_borrowed is not a bool.
+
+        title = "Book_Name"
+        author = "Author_Name"
+        genre = Genre.FICTION
+        item_id = 1
+        is_borrowed = True      
+
+        #Act
+        with self.assertRaises(ValueError) as exception_context:
+            target = LibraryItem(item_id, title, author, genre, is_borrowed)     
+
+        #Assert
+        expected = "Is Barrowed must be a boolean value."
+        actual = str(exception_context.exception)
+
+        #If the expected and the exception raised match the test passes.
+        self.assertEqual(expected, actual)
+
+    def test_init_exception_when_item_id_is_not_int(self):
+        """Tests if the propper exception is raised
+        when item_id is not an int"""
+        #Arange
+        #These variables will be input into an instance of LibraryItem
+        #to test for a ValueError when item_id is not a int.
+
+        title = "Book_Name"
+        author = "Author_Name"
+        genre = Genre.FICTION
+        item_id = "Invalid input"
+        is_borrowed = True
+
+        #Act
+        with self.assertRaises(ValueError) as exception_context:
+            target = LibraryItem(item_id, title, author, genre, is_borrowed)
+
+        #Assert
+        expected = "Item Id must be numeric."
+        actual = str(exception_context.exception)
+
+        #If the expected and the exception raised match the test passes.
+        self.assertEqual(expected, actual)
+
+
     def test_init_exception_when_title_is_blank(self):
         """Tests if the propper exception is raised
         when title is blank"""
@@ -44,11 +94,13 @@ class TestLibraryItem(unittest.TestCase):
         title = " "
         author = "Author Name"
         genre = Genre.NON_FICTION
+        item_id = 1
+        is_borrowed = True
 
         #Act
         #Test fails if a ValueError is not raised when target is initialized.
         with self.assertRaises(ValueError) as exception_context:
-            target = LibraryItem(title, author, genre)
+            target = LibraryItem(item_id, title, author, genre, is_borrowed)
 
         #Assert
         expected = "Title cannot be blank."
@@ -68,11 +120,13 @@ class TestLibraryItem(unittest.TestCase):
         title = "Book_Name"
         author = " "
         genre = Genre.TRUE_CRIME
+        item_id = 1
+        is_borrowed = True
 
         #Act
         #Test fails if a ValueError is not raised when target is initialized.
         with self.assertRaises(ValueError) as exception_context:
-            target = LibraryItem(title, author, genre)
+            target = LibraryItem(item_id, title, author, genre, is_borrowed)
 
         #Assert
         expected = "Author cannot be blank."
@@ -91,11 +145,13 @@ class TestLibraryItem(unittest.TestCase):
         title = "Book Name"
         author = "Author Name"
         genre = "This is wrong"
-        
+        item_id = 1
+        is_borrowed = True
+
         #Act
         #Test fails if a ValueError is not raised when target is initialized.
         with self.assertRaises(ValueError) as exception_context:
-            target = LibraryItem(title, author, genre)
+            target = LibraryItem(item_id, title, author, genre, is_borrowed)
 
         #Assert
         expected = "Invalid Genre."
@@ -112,9 +168,10 @@ class TestLibraryItem(unittest.TestCase):
         title = "Book Name"
         author = "Author Name"
         genre = Genre.FICTION
-        target = LibraryItem(title, author, genre)
+        item_id = 1
+        is_borrowed = True
 
-        #Act
+        target = LibraryItem(item_id, title, author, genre, is_borrowed)
 
         #Assert
         #If the expected is the same as target.title the test passes.
@@ -129,9 +186,10 @@ class TestLibraryItem(unittest.TestCase):
         title = "Book Name"
         author = "Author Name"
         genre = Genre.FICTION
-        target = LibraryItem(title, author, genre)
+        item_id = 1
+        is_borrowed = True
 
-        #Act
+        target = LibraryItem(item_id, title, author, genre, is_borrowed)
 
         #Assert
         #If the expected is the same as target.author the test passes.
@@ -146,9 +204,10 @@ class TestLibraryItem(unittest.TestCase):
         title = "Book Name"
         author = "Author Name"
         genre = Genre.FICTION
-        target = LibraryItem(title, author, genre)
+        item_id = 1
+        is_borrowed = True
 
-        #Act
+        target = LibraryItem(item_id, title, author, genre, is_borrowed)
 
         #Assert
         #if the expected is the same as target.genre the test passes.
@@ -156,3 +215,39 @@ class TestLibraryItem(unittest.TestCase):
         self.assertEqual(expected, target.genre)
 
 
+    def test_is_borrowed_returns_current_state(self):
+        """Tests if is_borrowed returns the current state correctly"""
+
+        #Arrange
+        #Variables used to create a the target instance with correct values.
+        title = "Book Name"
+        author = "Author Name"
+        genre = Genre.FICTION
+        item_id = 1
+        is_borrowed = True
+
+        target = LibraryItem(item_id, title, author, genre, is_borrowed)
+
+        #Assert
+        #if the expected is the same as target.is_borrowed the test passes.
+        expected = is_borrowed
+        self.assertEqual(expected, target.is_barrowed)
+
+
+    def test_item_id_returns_current_state(self):
+        """Tests if item_id returns the current state correctly"""
+        #Arrange
+        #Variables used to create a the target instance with correct values.
+
+        title = "Book Name"
+        author = "Author Name"
+        genre = Genre.FICTION
+        item_id = 1
+        is_borrowed = True
+
+        target = LibraryItem(item_id, title, author, genre, is_borrowed)
+
+        #Assert
+        #if the expected is the same as target.item_id the test passes.
+        expected = item_id
+        self.assertEqual(expected, target.item_id)
