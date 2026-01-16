986d1ec2fbc6de5c71227874639bcb9582a7d0b3
Hudson Drozdowski <hudsondrozdowski@gmail.com>
2026-01-16 01:52:58 -0600

    Part 2 Step 1: Updated LibraryItem based on the new class diagram, added two attributes & accessor methods.

--- library_item/library_item.py
+++ library_item/library_item.py
@@ -9,15 +9,21 @@ __version__ = "3.13.7"
 
 class LibraryItem:
 
-    def __init__(self, title : str, author : str, genre : Genre):
+    def __init__(self, item_id : int, title : str, author : str, genre : Genre, is_borrowed : bool):
         """
         Initializes a new instance of the LibraryItem class.
 
         args:
+            item_id (int): The id representing the library item
             title (str): The title of the library item
             author (str): The title of the author of the library item
-            genre: (Genre): The genre of the library item
+            genre (Genre): The genre of the library item
+            is_barrowed (bool): represents if the item is barrowed or not
         """
+
+        #If item id is not an int raises a ValueError
+        if not isinstance(item_id, int):
+            raise ValueError("Item Id must be numeric.")
 
         #If the length of the title is empty raises a ValueError
         if len(title.strip()) == 0:
@@ -31,13 +37,27 @@ class LibraryItem:
         if not isinstance(genre, Genre):
             raise ValueError("Invalid Genre.")
 
+        #If is barrowed is not a bool raises a ValueError
+        if not isinstance(is_borrowed, bool):
+            raise ValueError("Is Barrowed must be a boolean value.")
+
         #Sets the entered attributes 
+        self.__item_id = item_id
         self.__title = title
         self.__author = author
         self.__genre = genre
+        self.__is_borrowed = is_borrowed
 
+    #Item Id accessor
+    @property
+    def item_id(self) -> int:
+        """gets: the library items item id
+        
+        returns: the items id"""
 
-    #Title property
+        return self.__item_id
+
+    #Title accessor
     @property
     def title(self) -> str:
         """gets: the title of the librabary item
@@ -45,7 +65,7 @@ class LibraryItem:
         returns: the library items title"""
         return self.__title
     
-    #Author property
+    #Author accessor
     @property
     def author(self) -> str:
         """gets: the name of the author of the library item
@@ -53,7 +73,7 @@ class LibraryItem:
         returns: the name of the library items author"""
         return self.__author
     
-    #Genre property
+    #Genre accessor
     @property
     def genre(self) -> Genre:
         """gets: The Genre of the library item
@@ -61,3 +81,10 @@ class LibraryItem:
         returns: The genre of the library item"""
         return self.__genre
      
+    #Is Barrowed acessor
+    @property
+    def is_barrowed(self) -> bool:
+        """gets: The value of the bool is_barrowed
+        
+        returns: The value of the bool is_barrowed"""
+        return self.__is_borrowed
